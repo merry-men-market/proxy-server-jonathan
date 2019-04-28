0a3a584ee7f6e0dd6586f5a074c89aea2d1d5ffc
jnthnch <jchu127@gmail.com>
2019-04-27 20:33:35 -0700

    Implement proxy server for newrelic testing

--- proxy/server.js
+++ proxy/server.js
@@ -1,17 +1,35 @@
+const newrelic = require('newrelic');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const morgan = require('morgan');
+// const morgan = require('morgan');
+const axios = require('axios');
 const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.locals.newrelic = newrelic;
+
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(morgan('dev'));
+app.use('/stocks/:id', express.static(path.join(__dirname, 'public')));
+
+app.get('/api/stocks/:id', (req, res) => {
+  // http://13.57.217.200:3001/api/hostels/5
+  axios.get(`http://localhost:8080/api/stocks/${req.params.id}`)
+    .then(function (response) {
+      res.status(200).send(response.data);
+    })
+    .catch(function (error) {
+      res.send(error)
+    });
+});
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
